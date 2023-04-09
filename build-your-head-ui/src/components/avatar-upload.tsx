8678c48ca650518c66072d2c5177fb6721eba8c6
maksim.shila <maksim.shila@gmail.com>
2023-04-09 13:11:46 +0300

    Add product avatar preview

--- build-your-head-ui/src/components/avatar-upload.tsx
+++ build-your-head-ui/src/components/avatar-upload.tsx
@@ -1,9 +1,36 @@
+import React, { ChangeEvent } from "react";
 import styles from "./avatar-upload.module.css";
 
 export const AvatarUpload = () => {
+
+    const [imageName, setImageName] = React.useState<string>();
+    const [imageUrl, setImageUrl] = React.useState<string | null>(null);
+
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files == null) {
+            return;
+        }
+
+        const file = files[0];
+        if (file == null) {
+            return;
+        }
+
+        setImageName(file.name);
+        setImageUrl(URL.createObjectURL(file));
+    }
+
     return (
-        <div className={styles.container} onClick={_ => console.log("Upload!")}>
-            <button className={styles.plusButton} onClick={e => e.preventDefault()} />
-        </div>
+        <label className={styles.container}>
+            {imageUrl ?
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src={imageUrl} alt={imageName} />
+                </div>
+                :
+                <span className={styles.plusButton} />
+            }
+            <input hidden type="file" accept=".jpg, .jpeg, .png" onChange={onChange} />
+        </label>
     );
 }
