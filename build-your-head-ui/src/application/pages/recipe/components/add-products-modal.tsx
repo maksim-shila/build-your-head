7ff1ca84ac8dfaa928288dae4664268fa72da559
Maksim Shyla <maksim.shila@gmail.com>
2023-05-23 19:34:51 +0300

    minor

--- build-your-head-ui/src/application/pages/recipe/components/add-products-modal.tsx
+++ build-your-head-ui/src/application/pages/recipe/components/add-products-modal.tsx
@@ -1,4 +1,8 @@
-import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap"
+import { List, ListInlineItem, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap"
+import { Product } from "../../../../api/models"
+import React from "react";
+import { useLoader } from "../../../../hooks/loader";
+import { GlobalContext } from "../../../context/global-context";
 
 interface Props {
     isOpen: boolean,
@@ -6,11 +10,31 @@ interface Props {
 }
 
 export const AddProductsModal: React.FC<Props> = ({ isOpen, toggle }) => {
+
+    const { $api } = React.useContext(GlobalContext);
+
+    const [products, setProducts] = React.useState<Product[]>([]);
+
+    React.useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    const fetchProducts = useLoader(async () => {
+        const response = await $api.Product.getAll().invoke();
+        if (response.success && response.data) {
+            setProducts(response.data)
+        }
+    })
+
     return (
         <Modal id="productModal" isOpen={isOpen} toggle={toggle} size="lg">
             <ModalHeader toggle={toggle}>Add Products</ModalHeader>
             <ModalBody>
-                <div>List of products (TODO)</div>
+                <List>
+                    {products.map(product => (
+                        <ListInlineItem>{product.name}</ListInlineItem>
+                    ))}
+                </List>
                 <div>Selected products (TODO)</div>
             </ModalBody>
             <ModalFooter>
